a4f901f5a40916d5396c151ef174df1c60f1794e
IMart <i_mart@mail.ru>
2018-06-06 07:00:15 +0300

    go on

--- tomoimage/prep_dataset/canvasdriver.py
+++ tomoimage/prep_dataset/canvasdriver.py
@@ -6,19 +6,40 @@ class CanvasDriver:
     XMAX = 11
     YMAX = 11
     COLORMAX = 10
+    N_ROTATIONS = 10
 
     def __init__(self):
         self.canvas = canvas.Canvas(self.XMAX, self.YMAX)
 
-    def fillArea(self):
+    def putOriginalImageToStorage(self, canvas):
+        print("Original:")
+        canvas.printme()
+        
+    def putNextDatasetToStorage(self, newcanvas):
+        print("Next:")
+        newcanvas.printme()
+        print(newcanvas.get_dataset())
+        print()
+                
+    def fillTriangles(self):
         for x in range(0, self.N_TRIANGLES):
-            x_top = random.randint(0, self.XMAX / 2)
-            y_top = random.randint(0, self.YMAX / 2)
-            height = random.randint(2, self.XMAX / 2)
-            y0 = random.randint(0, self.YMAX / 2 - 1)
-            y1 = random.randint(self.YMAX / 2 + 1, self.YMAX)
+            x_top = random.randint(0, round(self.XMAX / 2))
+            y_top = random.randint(0, round(self.YMAX / 2))
+            height = random.randint(2, round(self.XMAX / 2))
+            y0 = random.randint(0, round(self.YMAX / 2) - 1)
+            y1 = random.randint(round(self.YMAX / 2) + 1, self.YMAX)
             value = random.randint(1, self.COLORMAX)
             self.canvas.draw_triangle(x_top, y_top, height, y0, y1, value)
+            self.putOriginalImageToStorage(self.canvas)
+        degree = 0.0
+        for nrot in range(0, self.N_ROTATIONS):
+            print(nrot)
+            degree = 360.0 / self.N_ROTATIONS * nrot
+            newcanvas = self.canvas.rotate_over_angle(round(degree))
+            self.putNextDatasetToStorage(newcanvas)
 
+    def prepareDataset(self):
+        self.fillTriangles()
+       
     def printme(self):
         self.canvas.printme()
--- tomoimage/prep_dataset/testcanvasdriver.py
+++ tomoimage/prep_dataset/testcanvasdriver.py
@@ -3,5 +3,4 @@ import canvasdriver
 
 if(1 == 1):    
     cv = canvasdriver.CanvasDriver()
-    cv.fillArea()
-    cv.printme()
+    cv.prepareDataset()
